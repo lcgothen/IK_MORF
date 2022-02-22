e9358c0fc78d305e41a909097cfac68a7d66975e
Leonor Gothen <lcgothen@gmail.com>
2022-02-22 09:31:13 +0000

    Add files

---
+++ catkin_ws/build/morf_IK/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/leonor/tese/catkin_ws/devel/lib/morf_IK/talker"
+  "/home/leonor/tese/catkin_ws/devel/lib/morf_IK/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/src/morf_IK/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/leonor/tese/catkin_ws/src/morf_IK/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/leonor/tese/catkin_ws/src/morf_IK/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/leonor/tese/catkin_ws/src/morf_IK:$ROS_PACKAGE_PATH"
