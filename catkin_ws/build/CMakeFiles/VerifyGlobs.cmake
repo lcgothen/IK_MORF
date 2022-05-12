8acffdb8d0b8407452109fdd928e3a20892cdb12
Leonor <lcgothen@gmail.com>
2022-05-12 12:15:09 +0200

    trying to include fann

--- catkin_ws/build/CMakeFiles/VerifyGlobs.cmake
+++ catkin_ws/build/CMakeFiles/VerifyGlobs.cmake
@@ -3,12 +3,8 @@
 cmake_policy(SET CMP0009 NEW)
 
 # fann_real at morf_ik/CMakeLists.txt:228 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/leonor/fann/src/*.a")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/leonor/tese/IK_MORF/catkin_ws/src/morf_ik/*.so")
 set(OLD_GLOB
-  "/home/leonor/fann/src/libdoublefann.a"
-  "/home/leonor/fann/src/libfann.a"
-  "/home/leonor/fann/src/libfixedfann.a"
-  "/home/leonor/fann/src/libfloatfann.a"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
@@ -16,25 +12,8 @@ if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
 endif()
 
 # fann_real at morf_ik/CMakeLists.txt:228 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/leonor/fann/src/*.so")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/leonor/tese/IK_MORF/catkin_ws/src/morf_ik/*.so.2")
 set(OLD_GLOB
-  "/home/leonor/fann/src/libdoublefann.so"
-  "/home/leonor/fann/src/libfann.so"
-  "/home/leonor/fann/src/libfixedfann.so"
-  "/home/leonor/fann/src/libfloatfann.so"
-  )
-if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
-  message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "/home/leonor/tese/IK_MORF/catkin_ws/build/CMakeFiles/cmake.verify_globs")
-endif()
-
-# fann_real at morf_ik/CMakeLists.txt:228 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "/home/leonor/fann/src/*.so.2")
-set(OLD_GLOB
-  "/home/leonor/fann/src/libdoublefann.so.2"
-  "/home/leonor/fann/src/libfann.so.2"
-  "/home/leonor/fann/src/libfixedfann.so.2"
-  "/home/leonor/fann/src/libfloatfann.so.2"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
