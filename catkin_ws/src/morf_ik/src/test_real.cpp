bf83cbb48d2d7ba38912482c17744aa914eaf89c
Leonor <lcgothen@gmail.com>
2022-05-12 09:12:51 +0200

    testing real position estimation

--- catkin_ws/src/morf_ik/src/test_real.cpp
+++ catkin_ws/src/morf_ik/src/test_real.cpp
@@ -41,9 +41,9 @@ int main(int argc, char **argv)
     angles FL, FR, ML, MR, BL, BR;
     angles auxML, auxMR, auxBL, auxBR;
     
-    posFL.x = 0;//-7.5776e-02;
-    posFL.y = +1.3090e-01;
-    posFL.z = -2.9912e-01;
+    // posFL.x = 0;//-7.5776e-02;
+    // posFL.y = +1.3090e-01;
+    // posFL.z = -2.9912e-01;
 
     stableML.x = -7.5776e-02;
     stableML.y = +1.7632e-01;
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
 
 
     // convert to leg frames
-    posFL = posFL.morf2FL(); 
+    // posFL = posFL.morf2FL(); 
     stableML = stableML.morf2ML(); 
     stableBL = stableBL.morf2BL(); 
     posFR = posFR.morf2FR(); 
@@ -75,7 +75,7 @@ int main(int argc, char **argv)
     stableBR = stableBR.morf2BR(); 
 
     // calculate IK parameters
-    FL.calcIK(posFL);
+    // FL.calcIK(posFL);
     ML.calcIK(stableML);
     BL.calcIK(stableBL);
     FR.calcIK(posFR);
