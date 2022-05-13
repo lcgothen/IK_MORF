7e1e8178687170a77ac0e4d59127710168132abd
Leonor <lcgothen@gmail.com>
2022-05-13 11:01:26 +0200

    smaller joint limits

--- catkin_ws/src/morf_ik/include/controller_real.cpp
+++ catkin_ws/src/morf_ik/include/controller_real.cpp
@@ -253,7 +253,7 @@ void CPG::cyclic(images *stereo)
 
 void CPG::walk(images stereo)
 {
-    float offset2 = 1.5, offset3 = -2.25/4;
+    float offset2 = 1.5, offset3 = -2.25/3;
     float d=0.07-stereo.target.x;//0.08-1.2*stereo.target.x;//-0.03-stereo.target.x;
 
     // joint limits from /gorobots/projects/C-CPGRBFN/CPGRBFN_BBO_v5/neural_controllers/morf/real/neutronController.cpp
