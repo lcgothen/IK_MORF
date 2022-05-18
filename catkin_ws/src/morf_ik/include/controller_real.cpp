cf001b98d9f768b003c308f128a0d2c783b36df6
Leonor <lcgothen@gmail.com>
2022-05-18 11:20:44 +0200

    using intrinsic matrix for position estimation

--- catkin_ws/src/morf_ik/include/controller_real.cpp
+++ catkin_ws/src/morf_ik/include/controller_real.cpp
@@ -450,8 +450,8 @@ void images::blob()
         // float height = 2*target.z*tan(fov_y/2);
         float height = 800*width/848;
 
-        target.x = -keypointsL[0].pt.x/848*width+width/2;
-        target.y = -keypointsL[0].pt.y/800*height+height/2;
+        target.x = -keypointsL[0].pt.x/848*width+newMatrixL.at<double>(0,2)/848*width; //width/2;
+        target.y = -keypointsL[0].pt.y/800*height+newMatrixL.at<double>(1,2)/800*height;
 
         // float f=newMatrixL.at<double>(0,0); //focal_length
         // // std::cout << f << std::endl;
